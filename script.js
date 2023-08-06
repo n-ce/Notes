f378733a91b60d4d80fe483b8da4c7bc25f4bf0d
abrocks1120@gmail.com <abrocks1120@gmail.com>
2023-08-06 21:27:42 +0530

    added initial note adding functionality

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-add_button.addEventListener('click',()=>{
+
+
+function newNote(title){
 	const li = document.createElement('li');
+	li.textContent = title;
+	li.addEventListener('click', () => {
+		page.innerHTML = '';
+		page.classList.toggle('hide');
+		const heading = document.createElement('input');
+		heading.type = 'text';
+		heading.id = 'pageTitle';
+		heading.placeholder = title;
+		const body = document.createElement('textarea');
+		body.id = 'pageText';
+		body.placeholder = 'Enter text here'
+		page.append(heading, body);
+	})
 	document.querySelector('ul').appendChild(li)
-})
+}
+
+add_button.addEventListener('click', () => {
+	const title = prompt('Enter Title')
+	newNote(title);
+});
