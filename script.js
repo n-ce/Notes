1c79e3e7d4531a513409855dc25f1b05f2ffd5b0
abrocks1120@gmail.com <abrocks1120@gmail.com>
2023-08-06 21:48:44 +0530

    added close note button

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-
-
-function newNote(title){
+function newNote(title) {
 	const li = document.createElement('li');
 	li.textContent = title;
 	li.addEventListener('click', () => {
-		page.innerHTML = '';
-		page.classList.toggle('hide');
+		page.style.display = 'flex';
+		const closeBtn = document.createElement('button');
+		closeBtn.textContent = '<';
+		closeBtn.id = 'close_button';
+		closeBtn.addEventListener('click', () => {
+			page.style.display = 'none';
+			page.innerHTML = '';
+		})
 		const heading = document.createElement('input');
 		heading.type = 'text';
 		heading.id = 'pageTitle';
-		heading.placeholder = title;
+		heading.value = title;
+		const div = document.createElement('div');
+		div.append(closeBtn, heading);
 		const body = document.createElement('textarea');
 		body.id = 'pageText';
 		body.placeholder = 'Enter text here'
-		page.append(heading, body);
+		page.append(div, body);
 	})
 	document.querySelector('ul').appendChild(li)
 }
